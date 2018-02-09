9f9fa7f53e9967766c454eb520f7dc666db3eca5
wangxuqian <799357186@qq.com>
2018-02-09 19:54:00 +0800

    11111

--- ppp.php
+++ ppp.php
@@ -1,3 +1,54 @@
-<?php 
-echo 123
-echo 456
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>添加用户</title>
+<link rel="stylesheet" type="text/css" href="css/css.css" />
+<script type="text/javascript" src="js/jquery.min.js"></script>
+</head>
+<body>
+	<div id="pageAll">
+		<div class="pageTop">
+			<div class="page">
+				<img src="img/coin02.png" /><span><a href="#">首页</a>&nbsp;-&nbsp;<a
+					href="#">员工管理</a>&nbsp;-</span>&nbsp;员工添加
+			</div>
+		</div>
+		<div class="page ">
+			<!-- 会员注册页面样式 -->
+			<div class="banneradd bor">
+				<div class="baTopNo">
+					<span>员工添加</span>
+				</div>
+				<div class="baBody">
+					<div class="bbD">
+						<form action="#" method="post" enctype="multipart/form-data">
+						&nbsp;&nbsp;&nbsp;
+							&nbsp;&nbsp;<p>员工姓名：<input type="text" name="staff_name" class="input3" />&nbsp;&nbsp;员工头像：<input type="file" name="staff_photo" /></p>
+							&nbsp;&nbsp;<p>员工性别：<input type="radio" name="staff_sex" value='1' />男&nbsp;&nbsp;&nbsp;&nbsp;
+							<input type="radio" name="staff_sex" value='2' />女</p>
+							&nbsp;&nbsp;<p>入职时间：<input type="text" name="entry_time" class="input3" />&nbsp;&nbsp;婚姻状况：<input  class="input3" type="text" name="staff_marital_status" /></p>
+							&nbsp;&nbsp;<p>离职时间：<input type="text" name="resignation_time" class="input3" />&nbsp;&nbsp;毕业院校：<input  class="input3" type="text" name="staff_school" /></p>
+							&nbsp;&nbsp;<p>所属职位：<input type="text" name="staff_industry" class="input3" />&nbsp;&nbsp;学历背景：<input  class="input3" type="text" name="staff_education" /></p>
+							&nbsp;&nbsp;<p>所属级别：<input type="text" name="staff_runk" class="input3" />&nbsp;&nbsp;工作时间：<input  class="input3" type="text" name="staff_work_now" /></p>
+							&nbsp;&nbsp;<p>所属分组：<input type="text" name="staff_jurisdiction" class="input3" />&nbsp;&nbsp;员工民族：<input  class="input3" type="text" name="staff_nation" /></p>
+							&nbsp;&nbsp;<p>员工电话：<input type="text" name="staff_phone" class="input3" />&nbsp;&nbsp;户口所在：<input  class="input3" type="text" name="staff_registered_permanent" /></p>
+							&nbsp;&nbsp;<p>员工QQ：<input type="text" name="staff_qq" class="input3" />&nbsp;&nbsp;现居住地：<input  class="input3" type="text" name="staff_now_add" /></p>
+							&nbsp;&nbsp;<p>员工邮箱：<input type="text" name="staff_E-mail" class="input3" />&nbsp;&nbsp;政治面貌：<input  class="input3" type="text" name="staff_politics_status" /></p>
+							&nbsp;&nbsp;<p>员工籍贯：<input type="text" name="staff_native_place" class="input3" />&nbsp;&nbsp;员工工号：<input  class="input3" type="text" name="staff_sta_num" /></p>
+							&nbsp;&nbsp;<p>身份证号：<input type="text" name="staff_IDnumber" class="input3" />&nbsp;&nbsp;立即就职：<input  class="input3" type="text" name="staff_yn" /></p>
+
+							<p class="bbDP">
+								<input class="btn_ok btn_yes" type="submit" value="提交">
+								<input class="btn_ok btn_yes" type="reset" value="重置">
+							</p>
+						</form>
+					</div>
+				</div>
+			</div>
+
+			<!-- 会员注册页面样式end -->
+		</div>
+	</div>
+</body>
+</html>
